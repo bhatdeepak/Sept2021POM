80a4a2ed49a7d763d455b5a8d42993fbeb9b8617
bhatdeepaks <bhatdeepaks@yahoo.com>
2022-02-25 22:08:42 +0530

    fixed constants for errors

--- src/main/java/com/qa/opencart/util/Constants.java
+++ src/main/java/com/qa/opencart/util/Constants.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Constants {
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL = "https://demo.opencart.com/index.php?route=account/login";
 	public static final int DEFAULT_TIME_OUT = 5;
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
-	public static final String ACCOUNT_PAGE_HEADER = "Your Store11";
+	public static final String ACCOUNT_PAGE_HEADER = "Your Store";
 	public static final String LOGIN_ERROR_MSG = "No match for E-Mail Address and/or Password";
 	public static final String ACCOUNT_CREATION_SUCCESS = "Your Account Has Been Created";
 	public static final String REGISTER_SHEET_NAME ="registrationData";
